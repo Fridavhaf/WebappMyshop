ec0b2d8b048367d25f001075efb2abcecacd6e59
Fridavhaf <frhaf7974@oslomet.no>
2025-10-06 15:06:11 +0200

    Implement logging and error handling with Serilog

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Myshop.Models;
 using Myshop.ViewModels;
 using Myshop.DAL;
+using System.Runtime.CompilerServices;
 
 
 namespace Myshop.Controllers;
@@ -9,15 +10,23 @@ namespace Myshop.Controllers;
 public class ItemController : Controller
 {
     private readonly IItemRepository _itemRepository;
+    private readonly ILogger<ItemController> _logger;
 
-    public ItemController(IItemRepository itemRepository)
+    public ItemController(IItemRepository itemRepository, ILogger<ItemController>
+        logger)
     {
         _itemRepository = itemRepository;
+        _logger = logger;
     }
 
     public async Task<IActionResult> Table()
     {
         var items = await _itemRepository.GetAll();
+        if (items == null)
+        {
+            _logger.LogError("[ItemController] Item list not found while executing _itemRepository.GetAll()");
+            return NotFound("Item list not found");
+        }
         var itemsViewModel = new ItemsViewModel(items, "Table");
         return View(itemsViewModel);
     }
@@ -25,18 +34,26 @@ public class ItemController : Controller
     public async Task<IActionResult> Grid()
     {
         var items = await _itemRepository.GetAll();
+        if (items == null)
+        {
+            _logger.LogError("[ItemController] Item list not found while executing _itemRepository.GetAll()");
+            return NotFound("Item list not found");
+        }
         var itemsViewModel = new ItemsViewModel(items, "Grid");
         return View(itemsViewModel);
     }
-    
+
     public async Task<IActionResult> Details(int id)
     {
         var item = await _itemRepository.GetItemById(id);
         if (item == null)
-            return NotFound();
+        {
+            _logger.LogError("[ItemController] Item not found for the ItemId {ItemId:0000}", id);
+            return NotFound("Item not found for the ItemId");
+        }
         return View(item);
     }
-    
+
     [HttpGet]
     public IActionResult Create()
     {
@@ -48,11 +65,13 @@ public class ItemController : Controller
     {
         if (ModelState.IsValid)
         {
-            await _itemRepository.Create(item);
-            return RedirectToAction(nameof(Table));
+            bool returnOk = await _itemRepository.Create(item);
+            if (returnOk)
+                return RedirectToAction(nameof(Table));
         }
+        _logger.LogWarning("[ItemController] Item creation failed {@item}", item);
         return View(item);
-    }    
+    }
 
     [HttpGet]
     public async Task<IActionResult> Update(int id)
@@ -60,7 +79,8 @@ public class ItemController : Controller
         var item = await _itemRepository.GetItemById(id);
         if (item == null)
         {
-            return NotFound();
+            _logger.LogError("[ItemController] Item not found when updating the ItemId {ItemId:0000}", id);
+            return BadRequest("Item not found for the ItemId");
         }
         return View(item);
     }
@@ -70,9 +90,11 @@ public class ItemController : Controller
     {
         if (ModelState.IsValid)
         {
-            await _itemRepository.Update(item);
-            return RedirectToAction(nameof(Table));
+            bool returnOk = await _itemRepository.Update(item);
+            if (returnOk)
+                return RedirectToAction(nameof(Table));
         }
+        _logger.LogWarning("[ItemController] Item update failed {@item}", item);
         return View(item);
     }
 
@@ -82,7 +104,8 @@ public class ItemController : Controller
         var item = await _itemRepository.GetItemById(id);
         if (item == null)
         {
-            return NotFound();
+            _logger.LogError("[ItemController] Item not found for the ItemId {ItemId:0000}", id);
+            return BadRequest("Item not found for the ItemId");
         }
         return View(item);
     }
@@ -90,7 +113,12 @@ public class ItemController : Controller
     [HttpPost]
     public async Task<IActionResult> DeleteConfirmed(int id)
     {
-        var item = await _itemRepository.Delete(id);
+        bool returnOk = await _itemRepository.Delete(id);
+        if (!returnOk)
+        {
+            _logger.LogError("[ItemController] Item deletion failed for the ItemId {ItemId:0000}", id);
+            return BadRequest("Item deletion failed");
+        }
         return RedirectToAction(nameof(Table));
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Myshop.DAL;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +22,17 @@ builder.Services.AddDbContext<ItemDbContext>(options =>
 });
 
 builder.Services.AddScoped<IItemRepository, ItemRepository>();
+
+var loggerConfiguration = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.File($"Logs/app_{DateTime.Now:yyyyMMdd_HHmmss}.log");
+
+loggerConfiguration.Filter.ByExcluding(e => e.Properties.TryGetValue("SourceContext", out var value) &&
+                            e.Level == LogEventLevel.Information &&
+                            e.MessageTemplate.Text.Contains("Executed DbCommand"));
+
+var logger = loggerConfiguration.CreateLogger();
+builder.Logging.AddSerilog(logger);
 
 var app = builder.Build();
 
